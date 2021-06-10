fad3795586d9db3355e727dbeab57ab71c4a7a90
Jhonatan mendes <jhowfrombrazil@gmail.com>
2021-06-10 12:07:05 -0300

    add status component

--- components/Status.js
+++ components/Status.js
@@ -1,14 +1,49 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 const Status = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.wrapper}>
+      <View style={styles.item}>
+        <Text style={styles.itemText}>Rotweiller</Text>
+        <Image source={require("../assets/icons/pawprint-orange.png")} />
+      </View>
+
+      <View style={styles.item}>
+        <Text style={styles.itemText}>35 kg</Text>
+        <Image source={require("../assets/icons/weighing-machine-icon.png")} />
+      </View>
+
+      <View style={styles.item}>
+        <Text style={styles.itemText}>23.06.2020</Text>
+        <Image source={require("../assets/icons/confetti-icon.png")} />
+      </View>
+
+      <View style={styles.item}>
+        <Text style={styles.itemText}>Female</Text>
+        <Image source={require("../assets/icons/gender-icon.png")} />
+      </View>
     </View>
   );
 };
 
 export default Status;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: "#F5F5F5",
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "90%",
+    height: 76,
+  },
+  item: {
+    alignItems: "center",
+  },
+  itemText: {
+    fontSize: 10,
+    marginBottom: 9,
+  },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import HeaderProfile from "../components/HeaderProfile";
+import Status from "../components/Status";
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.screen}>
       <HeaderProfile />
+      <Status />
     </View>
   );
 };
